dabda1784fb9448e4dc2ac023ccc23dc11434bd1
Zane Wilson <wilsonzanea@gmail.com>
2022-05-04 19:44:34 -0400

    add some comments

--- comps/Forecast.jsx
+++ comps/Forecast.jsx
@@ -4,8 +4,11 @@ import { useEffect, useState } from "react";
 import styles from "../styles/Forecast.module.css";
 
 export default function Forecast() {
+    // weather is going to hold the forecast data
     const [weather, setWeather] = useState("");
 
+    // load the data from the api, key is hidden
+    // by placing the key in a backend function
     useEffect(() => {
         axios
             .get("/api/weather")
@@ -17,22 +20,29 @@ export default function Forecast() {
             });
     }, []);
 
+    // function for forecast here
     const DisplayWeather = () => {
+        // map over the state
         return weather.map((day, i) => {
+            // stops at the 6th iteration (0 based)
             if (i < 5) {
-                // create date from unix timestamp. convert to localDateString and 
+                // create date from unix timestamp. convert to localDateString and
                 // use the short version. undefined should follow local machine time (I think)
                 let date = new Date(day.dt * 1000).toLocaleDateString(
                     undefined,
                     { weekday: "short" }
                 );
+                // important variables from iteration object
                 const highTemp = Math.round(day.temp.max);
                 const lowTemp = Math.round(day.temp.min);
                 const desc = day.weather[0].description;
                 const weatherIcon = day.weather[0].icon;
                 const src = `http://openweathermap.org/img/wn/${weatherIcon}@2x.png`;
                 return (
-                    <div className={`${styles.card} ${i === 0 && styles.dark}`} key={day.dt}>
+                    <div
+                        className={`${styles.card} ${i === 0 && styles.dark}`}
+                        key={day.dt}
+                    >
                         <p className={styles.cardHeader}>{date}</p>
                         <Image
                             loader={() => src}
@@ -53,6 +63,8 @@ export default function Forecast() {
     };
 
     return (
-        <div className={styles.container}>{weather !== "" && <DisplayWeather />}</div>
+        <div className={styles.container}>
+            {weather !== "" && <DisplayWeather />}
+        </div>
     );
 }
